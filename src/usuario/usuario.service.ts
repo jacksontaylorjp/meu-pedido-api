7c5cba760913930d206d41bb750cc6e80fb21494
jacksontaylorjp <jacksontaylorjp@gmail.com>
2024-07-17 13:32:14 -0300

    feat: usando o bcrypt para criptgrar a senha

--- src/usuario/usuario.service.ts
+++ src/usuario/usuario.service.ts
@@ -3,14 +3,17 @@ import { CreateUsuarioDto } from './dto/create-usuario.dto';
 import { UpdateUsuarioDto } from './dto/update-usuario.dto';
 import { Usuario } from './entities/usuario.entity';
 import { PrismaService } from 'src/prisma/prisma.service';
-
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsuarioService {
   constructor(private prisma: PrismaService) { }
 
   async create(data: CreateUsuarioDto): Promise<Usuario> {
-    return this.prisma.usuario.create({ data });
+    const hashedSenha = await bcrypt.hash(data.senha, 10);
+    return this.prisma.usuario.create({ data:{
+      ...data, senha: hashedSenha
+    } });
   }
 
   findAll() {
@@ -40,5 +43,15 @@ export class UsuarioService {
         id: Number(id),
       },
     });
+  }
+
+  async verifyPassword(email: string, senha: string): Promise<boolean> {
+    const usuario = await this.prisma.usuario.findUnique({
+      where: { email },
+    });
+    if (!usuario) {
+      return false;
+    }
+    return bcrypt.compare(senha, usuario.senha);
   }
 }
