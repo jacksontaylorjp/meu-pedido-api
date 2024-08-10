7252a252ad585745558eab676bf0ed529f93ce92
jacksontaylorjp <jacksontaylorjp@gmail.com>
2024-08-10 19:10:39 -0300

    feat: config do cors

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,12 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+  app.enableCors({
+    origin: ['http://localhost:3001'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true,
+  });
   
   await app.listen(3000);
 }
