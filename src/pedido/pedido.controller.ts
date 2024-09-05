b585f4520e47b03471fc00ce3b952fcd80d06e0d
jacksontaylorjp <jacksontaylorjp@gmail.com>
2024-09-04 22:41:35 -0300

    feat: relatorio dia

--- src/pedido/pedido.controller.ts
+++ src/pedido/pedido.controller.ts
@@ -4,10 +4,14 @@ import { CreatePedidoDto } from './dto/create-pedido.dto';
 import { UpdatePedidoDto } from './dto/update-pedido.dto';
 import { AuthGuard } from 'src/auth/auth.guard';
 import { Pedido } from './entities/pedido.entity';
+import { UsuarioService } from 'src/usuario/usuario.service';
 
 @Controller('/pedido')
 export class PedidoController {
-  constructor(private readonly pedidoService: PedidoService) { }
+  constructor(
+    private readonly pedidoService: PedidoService,
+    private readonly usuarioService: UsuarioService
+  ) { }
 
   @UseGuards(AuthGuard)
   @Post()
@@ -30,7 +34,7 @@ export class PedidoController {
   ) {
     return this.pedidoService.findByData({ usuarioId, data });
   }
-  
+
   @UseGuards(AuthGuard)
   @Patch()
   update(@Body() updatePedidoDto: UpdatePedidoDto) {
@@ -39,9 +43,12 @@ export class PedidoController {
 
   @UseGuards(AuthGuard)
   @Get('/relatorio')
-  relatorioDia(
+  async relatorioDia(
     @Query('data') data: string
-  ){
-    return this.pedidoService.relatorioDia(data);
+  ) {
+    const response = await this.pedidoService.relatorioDia(data);
+    const relatorio = await Promise.all(response.map(async (res) => await this.usuarioService.findOne(res.usuarioId)));
+    const relatorioFinal = relatorio.map(({ senha, email, id, ...dados }) => dados);
+    return relatorioFinal;
   }
 }
--- src/pedido/pedido.module.ts
+++ src/pedido/pedido.module.ts
@@ -3,10 +3,11 @@ import { PedidoService } from './pedido.service';
 import { PedidoController } from './pedido.controller';
 import { UsuarioModule } from 'src/usuario/usuario.module';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { UsuarioService } from 'src/usuario/usuario.service';
 
 @Module({
   imports: [UsuarioModule, PrismaModule],
   controllers: [PedidoController],
-  providers: [PedidoService],
+  providers: [PedidoService, UsuarioService],
 })
 export class PedidoModule {}
